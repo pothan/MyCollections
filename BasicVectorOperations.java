54679cade18d3fd390f5d45e847fccc7c9c0bf47
pothan <pothanakumare@arkinsoftware.com>
2015-04-15 14:55:38 +0530

    Create BasicVectorOperations.java

---
+++ BasicVectorOperations.java
@@ -0,0 +1,25 @@
+import java.io.ObjectInputStream.GetField;
+import java.util.Vector;
+ 
+public class BasicVectorOperations {
+ 
+    public static void main(String a[]){
+	        Vector<String> vec = new Vector<String>();
+	        vec.add("First");
+	        vec.add("Sec");
+	        vec.add("3rd");
+	        vec.add("Fourth");
+	        vec.add("fifth");
+	        vec.add(1,"fifth");
+		 System.out.println(vec);    
+		 System.out.println("" +vec.get(2));
+		 System.out.println("" +vec.get(3));      
+		 System.out.println("" +vec.get(4));
+		 
+		 System.out.println("element at  " +vec.elementAt(3)); 
+		 System.out.println("" +vec.firstElement());
+		 System.out.println("" +vec.lastElement());
+        
+    }
+       
+}
